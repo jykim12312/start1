d532307496e0422f6ac16df06dce71b159e51960
jykim12312 <hal9001@nate.com>
2023-01-08 19:43:18 +0900

    Initial commit

---
+++ src/test/java/st/start1/Start1ApplicationTests.java
@@ -0,0 +1,13 @@
+package st.start1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Start1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
